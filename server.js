62ed89b8dcc1c0a927d952c0f8a950ae887ad8a8
Tomislav Tomcic <tomislav.tomcic@gmail.com>
2026-05-19 17:11:27 +0200

    Initial commit.

---
+++ server.js
@@ -0,0 +1,36 @@
+const path = require("path");
+const fastify = require("fastify");
+const fastifyStatic = require("@fastify/static");
+
+const PORT = process.env.PORT || 3000;
+const HOST =
+  process.env.HOST || (process.env.NODE_ENV === "production" ? "0.0.0.0" : "127.0.0.1");
+const PUBLIC_DIR = path.join(__dirname, "public");
+const app = fastify({
+  logger: process.env.NODE_ENV === "development",
+});
+
+app.register(fastifyStatic, {
+  root: PUBLIC_DIR,
+  prefix: "/",
+});
+
+app.get("/", async (request, reply) => {
+  return reply.sendFile("index.html");
+});
+
+app.setNotFoundHandler(async (request, reply) => {
+  return reply.code(404).type("text/html; charset=UTF-8").sendFile("404.html");
+});
+
+const start = async () => {
+  try {
+    await app.listen({ port: Number(PORT), host: HOST });
+    console.log(`tomislavhq running at http://localhost:${PORT}`);
+  } catch (err) {
+    app.log.error(err);
+    process.exit(1);
+  }
+};
+
+start();
